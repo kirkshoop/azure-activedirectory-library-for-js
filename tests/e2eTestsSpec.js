a47828e797fd663bc60d4424e86647a303985b52
Tushar Gupta <tushargupta51@gmail.com>
2016-11-11 19:09:40 -0800

    add more tests

--- tests/e2eTestsSpec.js
+++ tests/e2eTestsSpec.js
@@ -4,7 +4,7 @@
 /* global describe */
 
 describe('E2ETests', function () {
-    var appRootUrl, appToGoListUrl, appHomeUrl, appTodoListUrl, username, password, appSetupUrl, unassignedUsername, unassignedPassword, index;
+    var appRootUrl, username, password, appSetupUrl, unassignedUsername, unassignedPassword, index, mainWindow;
     var elementsArray = [
         'uirouter-nopopup-nohtml5-otherwise',
         'uirouter-nopopup-nohtml5-nootherwise',
@@ -25,28 +25,33 @@ describe('E2ETests', function () {
     ];
     appRootUrl = 'https://adaljstestapp.azurewebsites.net/'; //'https://adaljsnonangularapp.azurewebsites.net/'; //'http://localhost:44332/';
     appSetupUrl = appRootUrl + 'setup.html';
-    appHomeUrl = appRootUrl + '#/Home';
-    appToGoListUrl = appRootUrl + '#/ToGoList';
-    appTodoListUrl = appRootUrl + '#/TodoList'
     username = "victor@tushartest.onmicrosoft.com";
     password = "user@1992";
     unassignedUsername = "pandu@tushartest2.onmicrosoft.com";
     unassignedPassword = "user@1992";
+    mainWindow = null;
 
     var loadSetupPage = function () {
         browser.ignoreSynchronization = true;
         browser.get(appSetupUrl);
-        var EC = protractor.ExpectedConditions;
-        browser.wait(EC.titleContains('Angular'), 3000, 'setup page not loaded');
+        browser.wait(protractor.ExpectedConditions.titleContains('Angular'), 3000, 'setup page not loaded');
     }
 
-    var loginMethod = function (valUserName, valPassword, newUser, processResult) {
+    var loginMethod = function (valUserName, valPassword, otherUser, processResult, isPopup) {
+        if (isPopup) {
+            browser.getAllWindowHandles().then(function (handles) {
+                mainWindow = handles[0];
+                browser.switchTo().window(handles[1]);
+                browser.executeScript('window.focus();');
+            });
+        }
+
         element(by.id('cred_userid_inputtext')).isDisplayed().then(function (isDisplayed) {
             if (isDisplayed) {
                 element(by.id('cred_userid_inputtext')).sendKeys(valUserName);
                 sendLoginRequest(valPassword, processResult);
             }
-            else if (newUser) {
+            else if (otherUser) {
                 element(by.id('use_another_account_link')).click().then(function () {
                     element(by.id('cred_userid_inputtext')).sendKeys(valUserName);
                     sendLoginRequest(valPassword, processResult);
@@ -62,9 +67,15 @@ describe('E2ETests', function () {
 
     var sendLoginRequest = function (valPassword, processResult) {
         element(by.id('cred_password_inputtext')).sendKeys(valPassword);
-        browser.sleep(2000);
-        element(by.id('cred_sign_in_button')).click().then(function () {
-            processResult();
+        var signInButton = element(by.id('cred_sign_in_button'));
+        browser.wait(protractor.ExpectedConditions.elementToBeClickable(signInButton), 1000).then(function () {
+            signInButton.click().then(function () {
+                if (mainWindow !== null) {
+                    browser.switchTo().window(mainWindow);
+                    mainWindow = null;
+                }
+                processResult();
+            });;
         });
     };
 
@@ -81,28 +92,47 @@ describe('E2ETests', function () {
         });
     }
 
-    for (index = 4; index < 8; index++) {
+    for (index = 0; index < 16; index++) {
         (function (elementId) {
 
-            it('tests login button ', function () {
+            var isPopUp = elementId.indexOf('-popup-') > -1 ? true : false;
+
+            it('tests login button', function () {
                 loadSetupPage();
                 element(by.id(elementId)).click().then(function () {
+                    var startPageUrl;
+                    browser.executeScript('return window.location.href').then(function (url) {
+                        startPageUrl = url;
+                    });
+
+                    browser.sleep('500');
+
+                    var homePageInputTextElementPresent = false;
+                    if (isPopUp && elementId.indexOf('-otherwise') > -1) {
+                        expect(element(by.id('homeInputText')).isPresent()).toBe(true);
+                        homePageInputTextElementPresent = true;
+                        element(by.id('homeInputText')).sendKeys('test input');
+                    }
+
                     element(by.id('loginButton')).click().then(function () {
                         var expectedResult = function () {
-                            expect(browser.getCurrentUrl()).toContain(appRootUrl);
+                            expect(browser.getCurrentUrl()).toBe(startPageUrl);
                             expect(browser.executeScript(function () {
                                 return window.sessionStorage.getItem('adal.idtoken');
                             })).not.toBe(null);
-                            //browser.setLocation('UserData');
-                            //browser.getCurrentUrl().then(function (url) {
-                            //    expect(url).toContain('UserData');
-                            //    var ele = element(by.exactBinding('userInfo.userName'));
-                            //    ele.getText().then(function (text) {
-                            //        console.log(text);
-                            //    });
-                            //});
+
+                            if (isPopUp && homePageInputTextElementPresent) {
+                                expect(element(by.id('homeInputText')).getAttribute('value')).toBe('test input');
+                            }
+                            browser.sleep('1000');
+                            browser.setLocation('UserData');
+                            browser.sleep('500');
+                            element(by.exactBinding('userInfo.userName')).getText().then(function (text) {
+                                expect(text.toLowerCase()).toBe(username);
+                            });
+                            expect(element(by.exactBinding('userInfo.isAuthenticated')).getText()).toBe('true');
                         }
-                        loginMethod(username, password, false, expectedResult);
+                        loginMethod(username, password, false, expectedResult, isPopUp);
                     });
                     logoutMethod();
                 });
@@ -111,26 +141,39 @@ describe('E2ETests', function () {
             it('tests that navigating to protected route triggers login', function () {
                 loadSetupPage();
                 element(by.id(elementId)).click().then(function () {
+                    var expectedUrl;
+                    if (isPopUp) {
+                        expectedUrl = elementId.indexOf('-html5-') > -1 ? appRootUrl + 'TodoList' : appRootUrl + '#/TodoList';
+                    }
+                    else {
+                        browser.executeScript('return window.location.href').then(function (url) {
+                            expectedUrl = url;
+                        });
+                    }
                     element(by.id('todoListOption')).click().then(function () {
                         var expectedResult = function () {
-                            expect(browser.getCurrentUrl()).toContain(appRootUrl);
+                            expect(browser.getCurrentUrl()).toBe(expectedUrl);
                             expect(browser.executeScript(function () {
                                 return window.sessionStorage.getItem('adal.idtoken');
                             })).not.toBe(null);
                         }
-                        loginMethod(username, password, false, expectedResult);
+                        loginMethod(username, password, false, expectedResult, isPopUp);
                     });
                     logoutMethod();
                 });
             });
 
-            it('tests login with an unassigned user ', function () {
+            it('tests login with an unassigned user', function () {
                 loadSetupPage();
                 element(by.id(elementId)).click().then(function () {
+                    var startPageUrl;
+                    browser.executeScript('return window.location.href').then(function (url) {
+                        startPageUrl = url;
+                    });
                     element(by.id('loginButton')).click().then(function () {
                         var expectedResult = function () {
                             browser.getCurrentUrl().then(function (url) {
-                                expect(url).toContain(appRootUrl);
+                                expect(url).toBe(startPageUrl);
                                 expect(element(by.id('logoutButton')).isDisplayed()).toBe(false);
                                 browser.executeScript(function () {
                                     return {
@@ -143,7 +186,7 @@ describe('E2ETests', function () {
                                 });
                             });
                         }
-                        loginMethod(unassignedUsername, unassignedPassword, true, expectedResult);
+                        loginMethod(unassignedUsername, unassignedPassword, true, expectedResult, isPopUp);
                     });
                     logoutMethod();
                 });
